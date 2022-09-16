789bedfcadcfdc7857f9c8da5fe3f068bf3b2197
TaljaouiOssama <taljaouiossama@gmail.com>
2022-09-16 15:11:44 +0100

    dao service

---
+++ src/main/java/com/ossama/gestioncinema/GestionCinemaApplication.java
@@ -0,0 +1,29 @@
+package com.ossama.gestioncinema;
+
+import com.ossama.gestioncinema.services.IInitService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication @AllArgsConstructor
+public class GestionCinemaApplication implements CommandLineRunner {
+    private IInitService initService;
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCinemaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        initService.initCities();
+        initService.initCinemas();
+        initService.initTheaters();
+        initService.initSeats();
+        initService.initCategories();
+        initService.initMovies();
+        initService.initMoviesProjection();
+        initService.initSessions();
+        initService.initTickets();
+
+    }
+}
